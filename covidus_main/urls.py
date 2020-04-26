ebfc66b52eb1c052cbd0e4873f48153df3894fe6
reechee <richardokonicha@gmail.com>
2020-04-26 04:42:33 +0100

    landing page and entr view

--- covidus_main/urls.py
+++ covidus_main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from covidus_main import views
 
+app_name = "covidus_main"
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path("enter", views.enter, name="enter")
 ]
--- covidus_main/views.py
+++ covidus_main/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "covidus_main/home.html")
+
+
+def enter(request):
+    request_user = request._post['username']
+    context = {
+        "user": request_user
+    }
+    return render(request, "covidus_main/enter.html", context=context)
